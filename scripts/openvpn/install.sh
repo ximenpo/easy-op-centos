1087e9ac888abbc1787aeaef1a11e519e86274d2
ximenpo <ximenpo@jiandan.ren>
2016-12-31 15:05:45 +0800

    add scripts/openvpn/install.sh, and uniform files’ names.

---
+++ scripts/openvpn/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "${1}" = "" ] ;then
+    host=${EASY_OP_SSH_HOST}
+else
+    host=${1}
+fi
+
+bash    scripts/run-script.sh   ${host}   files/openvpn/install.sh
+bash    scripts/conf-update.sh  ${host}
+bash    scripts/run-script.sh   ${host}   files/openvpn/install.sh

e89c1c8a6215f2f4ae54e184a7444e8bd8ef913f
ximenpo <ximenpo@jiandan.ren>
2017-01-05 17:35:34 +0800

    iptables support [ip6tables was not supported].

---
+++ files/iptables/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+yum list installed  iptables   >/dev/null  ||   yum     install -y  iptables
+
+which systemctl > /dev/null && {
+    yum list installed  iptables-services   >/dev/null  ||   yum     install -y  iptables-services
+    [ "$(systemctl is-active firewalld.service)" = "active" ] && systemctl   stop   firewalld.service
+    [ "$(systemctl is-enabled firewalld.service)" = "enabled" ] && systemctl   disable   firewalld.service
+}
+
+which systemctl > /dev/null || {
+    chkconfig   firewalld.servic    off
+    service     firewalld           stop
+}
+
+exit    0

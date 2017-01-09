ef1e092acd46dfbd7eb1e519208ee98f1615fbed
ximenpo <ximenpo@jiandan.ren>
2017-01-09 17:25:27 +0800

    some modules changed to use the new _init.sh initialize.

--- scripts/_init.sh
+++ scripts/_init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # EO_GROUP means the server group
 if [ -n "${EASYOP_GROUP}" ]; then
     EO_GROUP="${EASYOP_GROUP}"
@@ -10,8 +12,10 @@ fi
 # EO_HOST means the target server
 if [ -n "${1}" ] && [ -e "${EASYOP_GROUP}/${1}" ]; then
     EO_HOST="${1}"
+    shift
 else
     EO_HOST="${EASYOP_HOST}"
 fi
 
-set -e
+# EO_CONF means the host's conf dir
+EO_CONF="${EO_GROUP}/${EO_HOST}"

0eacb23095539f62fac145f998b9e947fbccd9ee
ximenpo <ximenpo@jiandan.ren>
2017-01-10 09:59:34 +0800

    add _init.sh for initialize working env. update ignore files.

---
+++ _init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#
+#   Usage:  source _init.sh [default_group] [default_host]
+#
+
+# install rsync
+command -v rsync > /dev/null 2>&1  ||  {
+    command -v yum > /dev/null 2>&1  &&  {
+        echo    'install rsync now ...'
+        yum     install -y  rsync
+        command -v   rsync > /dev/null 2>&1    ||  echo    '!!! rsync not found'
+    }
+}
+
+# init conf
+EASYOP_GROUP="${1}"
+EASYOP_HOST="${2}"
+
+if [ -z "${EASYOP_GROUP}" ]; then
+    EASYOP_GROUP=servers
+fi
+
+export  EASYOP_GROUP
+export  EASYOP_HOST
+
+if [ ! -e "${EASYOP_GROUP}" ]; then
+    echo    "WARN: directory for group [${EASYOP_GROUP}] not found"
+fi
+
+if [ ! -e "${EASYOP_GROUP}/${EASYOP_HOST}" ]; then
+    echo    "WARN: directory for server [${EASYOP_GROUP}/${EASYOP_HOST}] not found"
+fi

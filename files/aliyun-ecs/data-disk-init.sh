b23803004827bfddf3bc6ba186a525c448644971
ximenpo <ximenpo@jiandan.ren>
2017-03-29 22:39:52 +0800

    support  xvd? and vc? aliyun-disk prefix.

--- files/aliyun-ecs/data-disk-init.sh
+++ files/aliyun-ecs/data-disk-init.sh
@@ -8,12 +8,14 @@ findmnt -m --target ${DATA_DIR} > /dev/null 2>&1 && {
     echo    ${DATA_DIR} has been mounted
 }
 
-for label in b c d e f g h i j k l m n o p q r s t u v w x y z ;
+for prefix in xvd vd ;
 do
-    fdisk -l | grep /dev/xvd${label} > /dev/null 2>&1 && {
-        df | grep /dev/xvd${label}1 || {
-            echo    found /dev/xvd${label}
-            fdisk /dev/xvd${label} <<EOF
+    for label in b c d e f g h i j k l m n o p q r s t u v w x y z ;
+    do
+        fdisk -l | grep /dev/${prefix}${label} > /dev/null 2>&1 && {
+            df | grep /dev/${prefix}${label}1 || {
+                echo    found /dev/${prefix}${label}
+                fdisk /dev/${prefix}${label} <<'EOF'
 n
 p
 1
@@ -21,13 +23,14 @@ p
 
 wq
 EOF
-            mkfs.ext3 /dev/xvd${label}1
-            mkdir   -p  ${DATA_DIR}
-            cat /etc/fstab | grep ${DATA_DIR} > /dev/null 2>&1 || echo /dev/xvd${label}1 /data ext3 defaults,noatime,nodiratime,nodev 0 0 >> /etc/fstab
-            findmnt -m --target ${DATA_DIR} > /dev/null 2>&1   || mount -a
-            break   1
+                mkfs.ext3 /dev/${prefix}${label}1
+                mkdir   -p  ${DATA_DIR}
+                cat /etc/fstab | grep ${DATA_DIR} > /dev/null 2>&1 || echo /dev/${prefix}${label}1 /data ext3 defaults,noatime,nodiratime,nodev 0 0 >> /etc/fstab
+                findmnt -m --target ${DATA_DIR} > /dev/null 2>&1   || mount -a
+                break   1
+            }
         }
-    }
+    done
 done
 
 exit    0
